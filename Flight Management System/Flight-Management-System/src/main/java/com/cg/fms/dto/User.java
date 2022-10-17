7b04b69a110960c5531e261284e7cdc1a4c10d7f
Gunjan Muni Mandal <89119390+jontyy27@users.noreply.github.com>
2022-10-17 13:02:50 +0530

    Add files via upload

---
+++ Flight Management System/Flight-Management-System/src/main/java/com/cg/fms/dto/User.java
@@ -0,0 +1,67 @@
+package com.cg.fms.dto;
+
+import java.math.BigInteger;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+  private String userType;
+  @Id
+  private BigInteger userId;
+  private String userName;
+  private String userPassword;
+  private BigInteger userPhone;
+  private String email;
+  
+  public User() {
+	  
+  }
+  public User(String userType, BigInteger userId, String userName, String userPassword, BigInteger userPhone, String email){
+    this.userType=userType;
+    this.userId=userId;
+    this.userName=userName;
+    this.userPassword=userPassword;
+    this.userPhone=userPhone;
+    this.email=email;
+  }
+  
+  public String getUserType(){
+    return userType;
+  }
+  public void setUserType(String userType){
+    this.userType=userType;
+  }
+  public BigInteger getUserID(){
+    return userId;
+  }
+  public void setUserId(BigInteger userId){
+     this.userId=userId;
+  }
+  public String getUserName(){
+    return userName;
+  }
+  public void setUserName(String userName){
+    this.userName=userName;
+  }
+  public String getUserPassword(){
+    return userPassword;
+  }
+  public void setUserPassword(String userPassword){
+    this.userPassword=userPassword;
+  }
+  public BigInteger getUserPhone(){
+    return userPhone;
+  }
+  public void setUserPhone(BigInteger userPhone){
+    this.userPhone=userPhone;
+  }
+  public String getEmail(){
+    return email;
+  }
+  public void setEmail(String email){
+    this.email=email;
+  }
+
+}
